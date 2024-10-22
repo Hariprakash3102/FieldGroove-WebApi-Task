5a31735eb76d995a090a1ef882ba9dfe54557b1a
Hariprakash3102 <hariprakash.srinivasan@concertidc.com>
2024-10-22 11:33:50 +0530

    Added the Validation in Login ,Register and CreateLead

---
+++ FieldGroove.MVC/Validation/LoginValidator.cs
@@ -0,0 +1,18 @@
+﻿
+using FieldGroove.MVC.Models;
+using FluentValidation;
+
+namespace Field_Groove.Domain.Validation
+{
+	public class LoginValidator : AbstractValidator<LoginModel>
+	{
+		public LoginValidator()
+		{
+			RuleFor(x => x.Email)
+				.NotEmpty().WithMessage("Email is Required")
+				.EmailAddress().WithMessage("Invalid Email Address");
+			RuleFor(x => x.Password)
+				.NotEmpty().WithMessage("Password is Required");
+		}
+	}
+}
